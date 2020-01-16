94ff5b3565f56c466a5dee81023421ecef01a388
Faldang <faldang@yahoo.com>
2020-01-16 18:51:58 +0100

    Implemented way to get ID for the Wishlist export

---
+++ WishlistExport.bat
@@ -0,0 +1,2 @@
+python WishlistExport.py
+pause
--- WishlistExport.py
+++ WishlistExport.py
@@ -2,6 +2,24 @@ import requests
 import time
 import datetime
 import csv
+import urllib.request
+
+
+def getuserid(name):
+    # Open wishlist based on profile name and turn into string
+    wurl = 'https://www.gog.com/u/' + name + '/wishlist'
+    fid = urllib.request.urlopen(wurl)
+    webpage = fid.read().decode('utf-8')
+    # Find where the ID starts and prepare to get the ID
+    index = webpage.find('userInfo":{"id":"') + 17
+    tid = ''
+    # Iterate through the page until you hit the end of the ID
+    while True:
+        if webpage[index] == '"':
+            break
+        tid += webpage[index]
+        index += 1
+    return tid
 
 
 def onepage():
@@ -28,14 +46,25 @@ def site(tn):
         onepage()
 
 
-# Prepare: Url, Params, Get first page json, Get total number of pages, open file, write CSV header
-url = 'https://www.gog.com/public_wishlist/975863384196/search'
+def get_conf():
+    # Get the user ID for the URL and the file path, from a provided text file
+    file = open('dudeId.txt', 'r')
+    ret_name = file.readline().rstrip('\n')  # First line is ID
+    ret_path = file.readline()  # Second line is file path
+    file.close()
+    return ret_name, ret_path
+
+
+# Prepare: Config, Url, Params, Get first page json, Get total number of pages, open file, write CSV header
+f_name, f_path = get_conf()
+f_id = getuserid(f_name)
+url = 'https://www.gog.com/public_wishlist/' + f_id + '/search'
 payload = {'hiddenFlag': '0', 'mediaType': '0', 'sortBy': 'title', 'page': '1'}
 r = requests.get(url, params=payload)
 time.sleep(1)
 rj = r.json()
 total = rj['totalPages']
-f = open(r'E:\Downloads\wishlist.csv', 'w', encoding='utf-8', newline='')
+f = open(f_path + 'wishlist.csv', 'w', encoding='utf-8', newline='')
 root = csv.writer(f)
 root.writerow(['Name', 'Discount', 'Amount'])
 site(total)
